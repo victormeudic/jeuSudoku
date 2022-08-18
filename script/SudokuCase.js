e24396b60122c13c19a9095eee59b4d3ecd00c5e
victor <victormeudic@gmail.com>
2022-08-18 18:16:24 +0200

    Fin de l'algo

--- script/SudokuCase.js
+++ script/SudokuCase.js
@@ -1,10 +1,23 @@
 export default class SudokuCase {
     constructor() {
-        this.estValeurInitiale = false
+      this.estValeurInitiale = false
+      this.initialiserValeurs()
     }
-
-    definirValeurInitiale() {
-        this.valeur = valeur
-        this.estValeurInitiale = true
+  
+    initialiserValeurs() {
+      this.valeursPossibles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
-}
+  
+    definirValeurInitiale(valeur) {
+      this.valeur = valeur
+      this.estValeurInitiale = true
+    }
+  
+    prochaineValeur() {
+      // Récupérer et Retirer la premiere valeur du tableau
+      this.valeur = this.valeursPossibles.shift()
+  
+      // Retourner cette valeur
+      return this.valeur
+    }
+  }
--- script/SudokuCurseur.js
+++ script/SudokuCurseur.js
@@ -1,50 +1,71 @@
 export default class SudokuCurseur {
     constructor() {
-    this.ligne = 0
-    this.col = 0
-    this.marche = 'avant'
+      this.ligne = 0
+      this.col = 0
+      this.marche = 'avant'
     }
-
-    // Fait progresser le curseur selin l'état de marche
+  
+    // Progresse le curseur selon l'état de marche,
+    // SOit en avant, soit en arriere
+    // Retourne si oui ou non le curseur a bien pu avancer
+    // ou reculer
     progresse() {
-        // Si marche avant
-        if (this.marche === "avant") {
-           // Avancer le curseur
-            return this.avance() 
-        } else {
-            // Si marche arrière
-            // Reculer le curseur
-            return thise.recule()
-        } 
+      // Si marche avant
+      if (this.marche === 'avant') {
+        // Avancer le curseur
+        return this.avance()
+      } else {
+        // Si la marche est arriere
+        // Reculer le curseur
+        return this.recule()
+      }
     }
-
+  
     avance() {
-        if (this.ligne === 8 && this.col === 8) {
-            return false
-        }
-
-        if (this.col === 8) {
-            this.ligne += 1
-            this.col = 0
-        } else {
-            this.col += 1
-        }
-
-        return true
+      // Si le curseur est au bout de la ligne
+      if (this.ligne === 8
+      && this.col === 8) {
+        // Retourner faux
+        return false
+      }
+      
+      // Sinon
+      // Si le curseur est en bout de ligne
+      if (this.col === 8) {
+        // Ligne suivante, colonne 0
+        this.ligne += 1
+        this.col = 0
+      } else {
+        // Si le curseur n'est pas en bout de ligne
+        // Meme ligne, colonne suivante
+        this.col += 1
+      }
+  
+      // Retourne si le curseur a bien avancé
+      return true
     }
-
+  
     recule() {
-        if (this.ligne === 0 && this.col === 0) {
-            return false
-        }
-
-        if (this.col === 0) {
-            this.ligne -= 1
-            this.col === 8
-        } else {
-            this.col -= 1
-        }
-
-        return true
+      // Si le curseur est en premiere position
+      if (this.ligne === 0
+      && this.col === 0) {
+        // Retourner faux 
+        return false
+      }
+      
+      // Sinon
+      if (this.col === 0) {
+        // Si la colonne est en premiere position
+        // Aller a la ligne precedente, derniere position
+        this.ligne -= 1
+        this.col = 8
+      } else {
+        // Sinon 
+        // Recule d'une colonne
+        this.col -= 1
+      }
+  
+      return true
+      // Retourne si le curseur a bien reculé
     }
-}
+  }
